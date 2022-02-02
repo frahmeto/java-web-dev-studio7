c405976ce1921f6e33c233e2b6de67f50cf6d869
Fidel Rahmeto <fidyod@gmail.com>
2022-02-02 12:55:04 -0600

    studio 8

--- src/org/launchcode/studio7/Main.java
+++ src/org/launchcode/studio7/Main.java
@@ -6,13 +6,13 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args){
-        ArrayList<String> tlc = new ArrayList<>(Arrays.asList("The Lord's Prayer","He's Got The Whole World In His Hands","Every Praise","same God","I worship you", "WayMaker", "Promises", "fill the Room", "Than you"));
+        ArrayList<String> lists = new ArrayList<>(Arrays.asList("The Lord's Prayer","He's Got The Whole World In His Hands","Every Praise","same God","I worship you", "WayMaker", "Promises", "fill the Room", "Than you"));
 
         // TODO: Declare and initialize a CD and a DVD object.
-        CD cd = new CD("Rooms for Squares", 700, 350, "CD-R", tlc);
-        DVD dvd = new DVD("pirates of the Caribbean", 5000,3000,"DVD-R", tlc);
+        CD cd = new CD("Rooms for Squares", 700, 350, "CD-R", lists);
+        DVD dvd = new DVD("pirates of the Caribbean", 5000,3000,"DVD-R", lists);
 
-        CD myCd = new CD("Thank you",300,250,"Cd-R",tlc);
+        CD myCd = new CD("Thank you",300,250,"Cd-R",lists);
 
         // TODO: Call each CD and DVD method to verify that they work as expected.
 
